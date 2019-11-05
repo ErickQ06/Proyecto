01225e4ba57db977892dc0d0cc9413f7392c7716
DianaEs11 <54159180+DianaEs11@users.noreply.github.com>
2019-11-05 00:05:21 -0600

    new

--- proyecto/WasteSmart/settings.py
+++ proyecto/WasteSmart/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
     'ENGINE': 'django.db.backends.postgresql_psycopg2',
     'NAME': 'wasteSmart',
     'USER': 'postgres',
-    'PASSWORD': 'admin123',
+    'PASSWORD': 'admin',
     'HOST': 'localhost',
     'PORT': '5432',
     }
